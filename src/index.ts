9165752b60f03d772657c379654bc19bdeff7b38
M. Afifudin <afif.x.udin@gmail.com>
2022-08-20 13:18:56 +0700

    feat: add useIsomorphicLayoutEffect hook

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export { default as useDebounceFn } from './use-debounce-fn';
 export { default as useFirstMount } from './use-first-mount';
 export { default as useInView } from './use-in-view';
 export { default as useIntersection } from './use-intersection';
+export { default as useIsomorphicLayoutEffect } from './use-isomorphic-layout-effect';
 export { default as useOnScreen } from './use-on-screen';
 export { default as usePrevious } from './use-previous';
 export { default as usePreviousDistinct } from './use-previous-distinct';
---
+++ src/use-isomorphic-layout-effect.ts
@@ -0,0 +1,21 @@
+import { useEffect, useLayoutEffect } from 'react';
+
+import { isBrowser } from './utils';
+
+/**
+ * Does exactly same as `useLayoutEffect`, but will use `useEffect` in server side to prevent warning from Next.js
+ *
+ * ---
+ *
+ * The signature is identical to `useEffect`, but it fires synchronously after all DOM mutations.
+ * Use this to read layout from the DOM and synchronously re-render. Updates scheduled inside
+ * `useLayoutEffect` will be flushed synchronously, before the browser has a chance to paint.
+ *
+ * Prefer the standard `useEffect` when possible to avoid blocking visual updates.
+ *
+ * If you’re migrating code from a class component, `useLayoutEffect` fires in the same phase as
+ * `componentDidMount` and `componentDidUpdate`.
+ */
+const useIsomorphicLayoutEffect = isBrowser ? useLayoutEffect : useEffect;
+
+export default useIsomorphicLayoutEffect;
