fce408cbe46599ad8d2d9bd504202ab52a2ba266
Muhammad Afifudin <afif.x.udin@gmail.com>
2023-04-06 09:18:46 +0700

    docs: fix baseUrl and update overview page

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/react-power-ups/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
--- documentation/src/pages/index.tsx
+++ documentation/src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout title={siteConfig.title}>
-      <Redirect to="/docs/overview" />
+      <Redirect to={`${siteConfig.baseUrl}docs/overview`} />
     </Layout>
   );
 }
